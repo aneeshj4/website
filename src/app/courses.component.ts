95bbe41eee38fa2016596254c6730ea45dddd3c2
aneeshjindal <aneesh.jindal@gmail.com>
2017-01-20 00:53:55 -0800

    Added coures, updated SecuredIn page and footer

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -7,15 +7,17 @@ import { Component } from '@angular/core';
 
 export class CoursesComponent {
     courses: Course[] = [
-        {number: "CS 61A", name: "The Structure and Interpretation of Computer Programs", year: "Fall 2015"},
-        {number: "EE 16A", name: "Designing Information Devices and Systems I", year: "Fall 2015"},
-        {number: "Physics 7A", name: "Physics for Scientists and Engineers", year: "Fall 2015"},
-        {number: "CS 61B", name: "Data Structures", year: "Spring 2016"},
-        {number: "EE 16B", name: "Designing Information Devices and Systems II", year: "Spring 2016"},
-        {number: "Math 53", name: "Multivariable Calculus", year: "Spring 2016"},
+        {number: "CS 170", name: "Efficient Algorithms and Intractable Problems", year: "Spring 2017"},
+        {number: "CS 61C", name: "Great Ideas in Computer Architecture (Machine Structures)", year: "Spring 2017"},
         {number: "CS 70", name: "Discrete Mathematics and Probability Theory", year: "Fall 2016"},
         {number: "CS 186", name: "Introduction to Databases", year: "Fall 2016"},
         {number: "Math 110", name: "Linear Algebra", year: "Fall 2016"},
+        {number: "CS 61B", name: "Data Structures", year: "Spring 2016"},
+        {number: "EE 16B", name: "Designing Information Devices and Systems II", year: "Spring 2016"},
+        {number: "Math 53", name: "Multivariable Calculus", year: "Spring 2016"},
+        {number: "CS 61A", name: "The Structure and Interpretation of Computer Programs", year: "Fall 2015"},
+        {number: "EE 16A", name: "Designing Information Devices and Systems I", year: "Fall 2015"},
+        {number: "Physics 7A", name: "Physics for Scientists and Engineers", year: "Fall 2015"},
     ]
 }
 
