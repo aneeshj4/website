3c7708839f52b3c113a60053d9571a61ef760eca
Aneesh Jindal <aneesh.jindal@gmail.com>
2019-03-12 19:04:46 -0700

    Added newer classes, updated about me page

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -7,6 +7,12 @@ import { Component } from '@angular/core';
 
 export class CoursesComponent {
     courses: Course[] = [
+        {number: "CS 189", name: "Introduction to Machine Learning", year: "Spring 2018"},
+        {number: "Data 100", name: "Principles and Techniques of Data Science", year: "Spring 2018"},
+        {number: "Physics 7B", name: "Heat, electricity, and magnetism", year: "Spring 2018"},
+        {number: "CS 162", name: "Operating Systems and System Programming", year: "Fall 2017"},
+        {number: "CS 161", name: "Computer Security", year: "Fall 2017"},
+        {number: "CS 188", name: "Introduction to Artificial Intelligence", year: "Fall 2017"},
         {number: "CS 170", name: "Efficient Algorithms and Intractable Problems", year: "Spring 2017"},
         {number: "CS 61C", name: "Great Ideas in Computer Architecture (Machine Structures)", year: "Spring 2017"},
         {number: "CS 70", name: "Discrete Mathematics and Probability Theory", year: "Fall 2016"},
@@ -17,7 +23,7 @@ export class CoursesComponent {
         {number: "Math 53", name: "Multivariable Calculus", year: "Spring 2016"},
         {number: "CS 61A", name: "The Structure and Interpretation of Computer Programs", year: "Fall 2015"},
         {number: "EE 16A", name: "Designing Information Devices and Systems I", year: "Fall 2015"},
-        {number: "Physics 7A", name: "Physics for Scientists and Engineers", year: "Fall 2015"},
+        {number: "Physics 7A", name: "Mechanics and wave motion", year: "Fall 2015"},
     ]
 }
 
